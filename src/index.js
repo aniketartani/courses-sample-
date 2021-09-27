6858ef219289c4b49e992beb32f2d810897a5dde
Aniket Artani <aniketartani25@gmail.com>
2021-09-27 12:35:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import ReactDom  from "react-dom";
 import App from "./components/app.jsx"
 
 ReactDom.render(<App />,document.getElementById("root"));
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
+
